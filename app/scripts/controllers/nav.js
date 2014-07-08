b885ccf047ec2f3261fdfc8c3dc19c23e600ee94
Sai Okabayashi <saipuck@gmail.com>
2014-07-08 14:45:04 -0500

    Ch 5 midpt.  new CSS.

---
+++ app/scripts/controllers/nav.js
@@ -0,0 +1,16 @@
+/**
+ * Created by saipuck on 7/8/14.
+ */
+
+'use strict';
+
+app.controller( 'NavCtrl', function( $scope, $location, Post ) {
+  $scope.post = { url: 'http://', title: '' };
+
+  $scope.submitPost = function() {
+     Post.create( $scope.post ).then( function( ref ) {
+       $location.path( '/posts/' + ref.name() );
+        $scope.post = { url: 'http://', title: '' };
+     })
+  }
+});
--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -7,13 +7,6 @@ app.controller( 'PostsCtrl', function($scope, $location, Post) {
   $scope.posts = Post.all;
   $scope.post = {url: 'http://', title: '' };
 
-  $scope.submitPost = function() {
-    Post.create( $scope.post ).then( function(ref) {
-      $location.path( 'posts/' + ref.name() );
-//      $scope.post = {url: 'http://', title: '' };
-    });
-  };
-
   $scope.deletePost = function(postId) {
     Post.delete( postId );
   };
